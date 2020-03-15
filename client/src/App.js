b1b5e4e6342bc01fed1cbb021d83650c52cf0545
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-15 18:08:46 +0100

    Changed some texts to rebuild the image

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Fib Calculator</h1>
+          <h1 className='App-title'>Fib Calculator v2</h1>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
         </header>
